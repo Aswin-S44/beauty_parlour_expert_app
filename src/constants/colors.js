ae622f3ac7f2fe77d8ada27a24d7483a63cf6429
Aswin-S-T <aswinswebart15@gmail.com>
2025-08-17 10:40:38 +0530

    profile screen done

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,3 +3,4 @@ export const secondaryColor = '#E5CEEB';
 export const GREY = '#F5F5F5';
 export const lightPurple = '#F3E5F5';
 export const bookedColor = '#EAEAEA';
+export const starColor = '#FFC107';
